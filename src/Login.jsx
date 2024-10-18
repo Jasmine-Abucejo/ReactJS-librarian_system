b840af926a4aa6e8ebd3a5f88680359513f1d3bc
Jasmine-Abucejo <abucejojasmine@gmail.com>
2024-10-18 19:28:28 +0800

    navigate:dashboard

---
+++ src/Login.jsx
@@ -0,0 +1,55 @@
+import "./App.css";
+import Home from "./Home";
+import { useForm } from "react-hook-form";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const umail = "jasmine@gmail.com";
+  const upass = "1234";
+  const onSubmit = (data) => {
+    console.log(data);
+    if (data.email !== umail || data.password !== upass) {
+      alert("Invalid email or password");
+    } else {
+      navigate("/home");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Login to your Account</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <p>Email is required</p>}
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && <p>Password is required</p>}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import "./index.css";
+import Login from "./Login";
+import Home from "./Home.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+  BrowserRouter,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
