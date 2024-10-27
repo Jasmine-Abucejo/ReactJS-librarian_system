3057480593175ef7883297601ed2b25a069c8e47
Jasmine-Abucejo <abucejojasmine@gmail.com>
2024-10-27 19:33:44 +0800

    add:table

--- src/Accounts.jsx
+++ src/Accounts.jsx
@@ -2,6 +2,27 @@ function Accounts() {
   return (
     <div>
       <h1>Accounts</h1>
+      <div>
+        <table
+          style={{
+            border: "solid black",
+            borderCollapse: "collapse",
+            width: 1000,
+          }}
+        >
+          <thead style={{ background: "teal" }}>
+            <tr>
+              <th>Name</th>
+
+              <th>Position</th>
+
+              <th>Email</th>
+
+              <th>Phone</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,36 +1,42 @@
-import Accounts from "./Accounts.jsx";
-import {
-  Outlet,
-  Route,
-  Routes,
-  useNavigate,
-  BrowserRouter,
-} from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
   const handleAccounts = () => {
     navigate("/home/accounts");
   };
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <button onClick={handleAccounts}>Accounts</button>
-
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100vw",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          width: "20vw",
+          borderRight: "solid",
+        }}
+      >
+        <h1>Home</h1>
+        <button onClick={handleAccounts}>Accounts</button>
+      </div>
       {/* This element will render either <DashboardMessages> when the URL is
           "/messages", <DashboardTasks> at "/tasks", or null if it is "/"
       */}
-      <Outlet />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "white",
+          width: "80vw",
+        }}
+      >
+        <Outlet />
+      </div>
     </div>
   );
 }
 
-// function Home() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Dashboard />}>
-//         <Route path="accounts" element={<Accounts />} />
-//       </Route>
-//     </Routes>
-//   );
-// }
 export default Home;
